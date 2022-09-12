61f61b87c15b0d01f9504a2a63f1b3376c6d1f31
Riasat Mahbub <Riasat.Mahbub@brainstation-23.com>
2022-09-12 16:56:38 +0600

    edit.php imports form correctly but result shows null

--- classes/form/edit.php
+++ classes/form/edit.php
@@ -20,17 +20,22 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once(__DIR__ . '/../../config.php');
-require_once($CFG->dirroot. '/local/message/classes/form/edit.php');
+//moodleform is defined in formslib.php
+require_once("$CFG->libdir/formslib.php");
 
-$PAGE->set_url(new moodle_url('/local/message/edit.php'));
-$PAGE->set_context(\context_system::instance());
-$PAGE->set_title('Edit');
+class edit extends moodleform {
+    //Add elements to form
+    public function definition() {
+        global $CFG;
+       
+        $mform = $this->_form; // Don't forget the underscore! 
 
-$mform = new edit();
-
-echo $OUTPUT->header();
-
-$mfrom->display();
-
-echo $OUTPUT->footer();
+        $mform->addElement('text', 'messagetext', 'Message Text'); // Add elements to your form.
+        $mform->setType('messagetext', PARAM_NOTAGS);                   // Set type of element.
+        $mform->setDefault('messagetext', 'Please enter a message');        // Default value.
+    }
+    //Custom validation should be added here
+    function validation($data, $files) {
+        return array();
+    }
+}
