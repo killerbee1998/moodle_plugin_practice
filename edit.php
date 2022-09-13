9f1f121618f260c5110b0d02753fd5db928904c3
Riasat Mahbub <Riasat.Mahbub@brainstation-23.com>
2022-09-13 15:53:24 +0600

    edit: message now  saves to db and redirects to manage page

--- edit.php
+++ edit.php
@@ -21,7 +21,8 @@
  */
 
 require_once(__DIR__ . '/../../config.php');
-require_once($CFG->dirroot. '/local/message/classes/form/edit_form.php');
+require_once($CFG->dirroot . '/local/message/classes/form/edit_form.php');
+global $DB;
 
 $actionUrl = new moodle_url('/local/message/edit.php');
 
@@ -33,15 +34,18 @@ $mform = new edit_form($actionUrl);
 
 //Form processing and displaying is done here
 if ($mform->is_cancelled()) {
-    //Handle form cancel operation, if cancel button is present on form
+  //Handle form cancel operation, if cancel button is present on form
 
-    redirect($CFG->wwwroot.'/local/message/manage.php', "You cancelled the message");
-
+  redirect($CFG->wwwroot . '/local/message/manage.php', "You cancelled the message");
 } else if ($fromform = $mform->get_data()) {
   //In this case you process validated data. $mform->get_data() returns data posted in form.
+  $recordtoinsert = new stdClass();
+  $recordtoinsert->messagetext = $fromform->messagetext;
+  $recordtoinsert->messagetype = $fromform->messagetype;
 
-} 
-
+  $DB->insert_record('local_message', $recordtoinsert);
+  redirect($CFG->wwwroot . '/local/message/manage.php', "You created a message");
+}
 echo $OUTPUT->header();
 
 $mform->display();
