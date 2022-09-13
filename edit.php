6cbf5fe7ff55c4ad5b21282697e8ad6e81dd7696
Riasat Mahbub <Riasat.Mahbub@brainstation-23.com>
2022-09-13 11:54:46 +0600

    edit: added error handling for form

--- edit.php
+++ edit.php
@@ -31,8 +31,27 @@ $PAGE->set_title('Edit');
 
 $mform = new edit_form($actionUrl);
 
+
+
 echo $OUTPUT->header();
 
-$mform->display();
 
+//Form processing and displaying is done here
+if ($mform->is_cancelled()) {
+    //Handle form cancel operation, if cancel button is present on form
+} else if ($fromform = $mform->get_data()) {
+  //In this case you process validated data. $mform->get_data() returns data posted in form.
+
+
+} else {
+  // this branch is executed if the form is submitted but the data doesn't validate and the form should be redisplayed
+  // or on the first display of the form.
+
+  //Set default data (if any)
+  $mform->set_data($toform);
+  //displays the form
+  $mform->display();
+}
+
+$mform->display();
 echo $OUTPUT->footer();
